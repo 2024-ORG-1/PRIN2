e65541417a5482a79997b0f09093e6f6ba45611d
gaspatt <matheusgaspar29.if@gmail.com>
2024-04-03 09:30:40 -0300

    feat:Página de contato #3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<script setup></script>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 
-<template></template>
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/contact">Contact</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      a
+    </nav>
+  </div>
+</template>
 
 <style scoped></style>
